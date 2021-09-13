fb87673c97da35f747b958a7c3c82603c67462b1
rhino2199 <55815552+rhino2199@users.noreply.github.com>
2021-09-13 16:41:28 -0500

    Player Created
    Added Player and Controller Script

---
+++ Circelli-ShooterGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+/****
+ * Created by: Ryan Circelli
+ * Date Created: Sept 13, 2021
+ * 
+ * Last Edited By:
+ * Last Updated Sept 13,2021
+ * 
+ * Description:Player control Movements
+ * 
+ */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
