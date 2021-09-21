ddf182302492a1ed73fa3edf0a05834a2d04f878
rhino2199 <rcircelli21@gmail.com>
2021-09-20 20:35:30 -0500

    Spawner
    Enemy Spawner made

---
+++ Circelli-ShooterGame/Assets/Scripts/Spawner.cs
@@ -0,0 +1,44 @@
+/****
+ * Created by: Ryan Circelli
+ * Date Created: Sept 20, 2021
+ * 
+ * Last Edited By: Ryan Circelli
+ * Last Updated Sept 20,2021
+ * 
+ * Description:Spawns Enemies
+ * 
+ */
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    //Variables
+
+    public float MaxRadius = 1f;
+    public float Interval = 5f;
+    public GameObject ObjToSpawn = null;
+    private Transform Origin = null;
+
+    private void Awake()
+    {
+        Origin = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Start()
+    {
+        InvokeRepeating("Spawn", 0f, Interval);
+    }
+
+    void Spawn()
+    {
+        if (Origin == null){return;}
+        Vector3 SpawnPos = Origin.position + Random.insideUnitSphere * MaxRadius;
+        SpawnPos = new Vector3(SpawnPos.x, 0f, SpawnPos.z);
+        Instantiate(ObjToSpawn, SpawnPos, Quaternion.identity);
+    }
+}
